f2cd2016d0b84c273399ac7eaa6aa6849c2edd4c
devjiraynor <jiraynor@naver.com>
2024-01-23 10:51:47 +0900

    2024.01.23. 10:50

---
+++ javaBasic/src/ch02/A_ClassAndObject.java
@@ -0,0 +1,19 @@
+package ch02;
+
+// 클래스: 공통된 속성과 기능을 하나로 정의해둔 것
+// [접근제어자] class 클래스명 { 속성, 기능 }
+// 클래스를 선언할 때는 클래스 외부에서 선언
+class ExampleClass1 {
+	// 속성: 클래스가 가질수 있는 정보나 상태
+	// 일반적으로 변수로 표현
+	int attribute1;
+	double attribute2;
+}
+
+public class A_ClassAndObject {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
