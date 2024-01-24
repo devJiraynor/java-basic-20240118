61f9fb7f582a6a811b749467542224b9a7fd7422
devjiraynor <jiraynor@naver.com>
2024-01-24 17:46:52 +0900

    2023.01.24. 16:50

--- javaBasic/src/ch02/A_JVM.java
+++ javaBasic/src/ch02/A_JVM.java
@@ -5,9 +5,10 @@ class ExampleClass2 {
 	static int number = 3;
 	double doubleNumber;
 	
-	void method(Sample1 arg) {
-		arg.data++;
-		System.out.println("method in : " + arg.data);
+	Sample1 method() {
+		Sample1 sample = new Sample1();
+		sample.data = -456;
+		return sample;
 	}
 	
 	static void staticMethod() {
@@ -17,6 +18,12 @@ class ExampleClass2 {
 
 class Sample1 {
 	int data;
+	
+	Sample1 () {}
+	
+	Sample1 (Sample1 sample) {
+		this.data = sample.data;
+	}
 }
 
 public class A_JVM {
@@ -27,14 +34,31 @@ public class A_JVM {
 		ExampleClass2 instance2 = new ExampleClass2();
 		
 		Sample1 sample = new Sample1();
-		sample.data = 0;
+		sample.data = 8;
 		
 		int number = 0;
 		instance1.doubleNumber = 9.9;
 		
-		instance1.method(sample);
+		sample = instance1.method();
 		
 		System.out.println(sample.data);
+		
+		int number2 = number;
+		number2++;
+		System.out.println(number);
+		System.out.println(number2);
+		
+		Sample1 sample2 = sample;
+		sample2.data++;
+		System.out.println(sample.data);
+		System.out.println(sample2.data);
+		
+		Sample1 sample3 = new Sample1(sample);
+		System.out.println(sample.data);
+		System.out.println(sample3.data);
+		sample3.data++;
+		System.out.println(sample.data);
+		System.out.println(sample3.data);
 		
 	}
 
