9ba9c96e78e78230894afd363814d74465d152c0
devjiraynor <jiraynor@naver.com>
2024-01-29 12:52:22 +0900

    2024.01.29. 12:50

--- javaBasic/src/ch03/B_Package.java
+++ javaBasic/src/ch03/B_Package.java
@@ -1,5 +1,9 @@
 package ch03;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class B_Package {
 
 	public static void main(String[] args) {
@@ -17,6 +21,27 @@ public class B_Package {
 		// toString 메서드: 특정 타입의 값을 문자열로 변환
 		String string = Integer.toString(123);
 		
+		// Collection Framework : java에서 데이터 구조에 대한 표준화된 기법을 제공 
+		
+		// List: 데이터를 저장할 때 중복 허용하고 순서가 존재함
+		List<Integer> arrayList; // int[] arrayList;
+		// *ArrayList: List 중에서 내부 배열을 사용하여 인덱스를 사용한 검색이 빠름
+		arrayList = new ArrayList<>();
+		// LinkedList: 이중 연결 기법을 사용하여 추가, 삭제가 빠름
+		// arrayList = new LinkedList<>();
+		
+		// add(): List 에 요소를 추가
+		// 추가할 요소의 데이터 타입은 반드시 제너릭으로 지정한 타입으로만 지정 가능
+		arrayList.add(10);
+		// arrayList.add("aaaa");
+		
+		// get(): List 의 특정 인덱스 요소에 접근
+		// List 인덱스 범위를 초과하는 인덱스를 지정할 수 없음
+		// 기본형 데이터타입의 변수로 반환값을 받을 경우 null 값을 받을 수 있음을 주의
+		int arrayElement = arrayList.get(0);
+		
 	}
 
 }
+
+
