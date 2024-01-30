f4c44d13849dd6d9845392d484658f2be3ed1d5f
devjiraynor <jiraynor@naver.com>
2024-01-30 12:12:05 +0900

    2024.01.30. 11:50

--- javaBasic/src/ch03/B_Package2.java
+++ javaBasic/src/ch03/B_Package2.java
@@ -1,6 +1,11 @@
 package ch03;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
@@ -72,6 +77,60 @@ public class B_Package2 {
 		}
 		
 		System.out.println(annuity);
+		
+		// Date 클래스: 날짜를 다루는 클래스
+		// Date 클래스의 대부분의 메서드는 JDK 1.1 부터 사용하지 않도록 권장하고 있음
+		Date dateNow = new Date();
+		System.out.println(dateNow);
+		System.out.println(dateNow.getDate());
+		
+		// LocalDateTime 클래스: Java 8 버전 이후부터 Date 클래스를 대체하기 위해 만들어진 클래스
+		// LocalDate 클래스, LocalTime 클래스, LocalDateTime 클래스
+		
+		// 현재 날짜 및 시간의 인스턴스 생성
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println(now);
+		// 특정 날짜 및 시간의 인스턴스 생성
+		LocalDateTime date1 = LocalDateTime.of(2024, 1, 18, 9, 0, 0);
+		System.out.println(date1);
+		
+		// 특정 요소 가져오기 getXXX()
+		int year = now.getYear();
+		System.out.println(year);
+		int month = now.getMonthValue();
+		System.out.println(month);
+		int day = now.getDayOfMonth();
+		System.out.println(day);
+		
+		// 특정 요소 변경하기 withXXX
+		// 변경한 값을 반환 함
+		now = now.withYear(2025).withHour(15);
+		System.out.println(now);
+		
+		// 날짜 계산하기
+		// 특정 요소에 값을 더하거나 빼기 plusXXX() / minusXXX()
+		// 변경한 값을 반환 함
+		now = now.plusMonths(2).minusDays(15);
+		System.out.println(now);
+		
+		// LocalDateTime을 Date 클래스로 변경
+		Date localToDate =  Date.from(now.atZone(ZoneId.systemDefault()).toInstant());
+		System.out.println(localToDate);
+		
+		// Date -> String
+		// SimpleDateFormat 클래스를 사용하여 지정한 포멧으로 문자열으로 변경
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+		String dateString = simpleDateFormat.format(localToDate);
+		System.out.println(dateString);
+		
+		// y: 연도, M: 월, d: 일, a: 오전오후, H: 시간(24), h: 시간(12), m: 분, s: 초, E: 요일
+		simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		dateString = simpleDateFormat.format(localToDate);
+		System.out.println(dateString);
+		
+		// LocalDateTime -> String
+		dateString = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+		System.out.println(dateString);
 		
 	}
 
